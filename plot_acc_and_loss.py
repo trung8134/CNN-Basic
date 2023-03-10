8d6fc506036717a35d280bb6019090b4310ea66b
Cao Duc Trung <113839491+trung8134@users.noreply.github.com>
2023-03-10 18:01:58 +0700

    Add files via upload

---
+++ plot_acc_and_loss.py
@@ -0,0 +1,24 @@
+# Đầu tiên ta lưu lịch sử train vào biến history
+history = model.fit_generator(aug.flow(X_train, y_train, batch_size=64), epochs=50, validation_data=aug.flow(X_test,y_test, batch_size=128))
+
+# Tiếp theo ta plot các thông số loss và acc ra
+def plot_model_history(model_history, acc='accuracy', val_acc='val_accuracy'):
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    axs[0].plot(range(1, len(model_history.history[acc]) + 1), model_history.history[acc])
+    axs[0].plot(range(1, len(model_history.history[val_acc]) + 1), model_history.history[val_acc])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1, len(model_history.history[acc]) + 1), len(model_history.history[acc]) / 10)
+    axs[0].legend(['train', 'val'], loc='best')
+    axs[1].plot(range(1, len(model_history.history['loss']) + 1), model_history.history['loss'])
+    axs[1].plot(range(1, len(model_history.history['val_loss']) + 1), model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1, len(model_history.history['loss']) + 1), len(model_history.history['loss']) / 10)
+    axs[1].legend(['train', 'val'], loc='best')
+    plt.show()
+    plt.savefig('roc.png')
+
+plot_model_history(history)
